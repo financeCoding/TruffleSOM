1400cc6c8fd0a5ce16595f038d77460f473ff141
Stefan Marr <git@stefan-marr.de>
2014-03-19 17:25:48 +0100

    Replaced with wrong block class in super class (on inlineing)
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/literals/BlockNode.java
+++ src/som/interpreter/nodes/literals/BlockNode.java
@@ -74,5 +74,11 @@ public class BlockNode extends LiteralNode {
     public SBlock executeSBlock(final VirtualFrame frame) {
       return universe.newBlock(blockMethod, frame.materialize(), this);
     }
+
+    @Override
+    public void replaceWithIndependentCopyForInlining(final Inliner inliner) {
+      SMethod forInlining = (SMethod) cloneMethod(inliner);
+      replace(new BlockNodeWithContext(forInlining, universe, contextLevel));
+    }
   }
 }
